484f86b5aede7b0f3bbcb73c81b6313fb9ffb4a1
chanisa01 <chanisa.iml@gmail.com>
2025-06-13 08:07:51 +0700

    edit navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,83 +26,40 @@ const Header = () => {
         </div>
       </div>
 
-      <nav className="navbar sticky-top navbar-expand-lg bg-white py-3 sticky-header">
-        <div className="container">
+        <div className="container-fluid nav-bar px-0 px-lg-4 py-lg-0">
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                  <Link className="navbar-brand p-0" to="/">
+                    {/* <img src="/Seal_of_KMUTNB.svg" alt="Logo" height="32" className="me-2" />
+                    IAU KMUTNB */}
+                    <img src="/internal.svg" alt="Logo" height="60" className="me-2" />
+                  </Link>
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span className="fa fa-bars"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <div className="navbar-nav mx-0 mx-lg-auto">
+                        <AboutDropdown/>
+                        <CommitteeDropdown/>
+                        <ReportDoc/>
+                        <li className="nav-item dropdown">
+                          <a className="nav-link dropdown-toggle" href="#" id="information" role="button" data-bs-toggle="dropdown">
+                            สารสนเทศ
+                          </a>
+                          <ul className="dropdown-menu custom-dropdown-menu" aria-labelledby="information">
+                            <li><Link className="dropdown-item" to="/AllActivity">กิจกรรมข่าว</Link></li>
+                            <li><Link className="dropdown-item" to="/DownloadDoc">เอกสารดาวน์โหลด</Link></li>
+                            <li><Link className="dropdown-item" to="/AllFAQ">คำถามที่พบบ่อย</Link></li>             
+                          </ul>
+                        </li>
 
-          {/* โลโก้ */}
-          <Link className="navbar-brand fw-bold text-primary d-flex align-items-center" to="/">
-            {/* <img src="/Seal_of_KMUTNB.svg" alt="Logo" height="32" className="me-2" />
-            IAU KMUTNB */}
-            <img src="/internal.svg" alt="Logo" height="60" className="me-2" />
-          </Link>
-
-          {/* ปุ่มสามขีดแสดงใน mobile */}
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#mainNavbar"
-            aria-controls="mainNavbar"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          {/* เมนูหลัก */}
-          <div className="collapse navbar-collapse justify-content-end" id="mainNavbar">
-            <div className="menu-container mt-3 mt-lg-0 w-100">
-              <ul className="navbar-nav flex-row flex-wrap align-items-center gap-3 px-3 menu-full-width">
-
-                {/* เกี่ยวกับหน่วยงาน */}
-                <AboutDropdown/>
-                {/* <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="aboutDropdown" role="button" data-bs-toggle="dropdown">
-                    เกี่ยวกับหน่วยงาน
-                  </a>
-                  <ul className="dropdown-menu custom-dropdown-menu" aria-labelledby="aboutDropdown">
-                    <li><Link className="dropdown-item" to="/History">ประวัติความเป็นมา</Link></li>
-                    <li><Link className="dropdown-item" to="/Vision">วิสัยทัศน์</Link></li>
-                    <li><Link className="dropdown-item" to="/Mission">พันธกิจ</Link></li>
-                    <li><Link className="dropdown-item" to="/Identity">อัตลักษณ์</Link></li>
-                    <li><Link className="dropdown-item" to="/Structure">โครงสร้าง</Link></li>
-                    <li><Link className="dropdown-item" to="/Personnal">บุคลากร</Link></li>                
-                  </ul>
-                </li> */}
-                {/* <li className="nav-item"><Link className="nav-link" to="/AllActivity">กิจกรรมข่าว</Link></li> */}
-
-                {/* คณะกรรมการตรวจสอบ */}
-                <CommitteeDropdown/>
-                {/* <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="committeeDropdown" role="button" data-bs-toggle="dropdown">
-                    คณะกรรมการตรวจสอบ
-                  </a>
-                  <ul className="dropdown-menu custom-dropdown-menu" aria-labelledby="committeeDropdown">
-                    <li><Link className="dropdown-item" to="/AuditCommittee">องค์ประกอบคณะกรรมการตรวจสอบ</Link></li>
-                  </ul>
-                </li> */}
-
-                {/* รายงานผลการดำเนินงาน */}
-                <ReportDoc/>
-
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="information" role="button" data-bs-toggle="dropdown">
-                    สารสนเทศ
-                  </a>
-                  <ul className="dropdown-menu custom-dropdown-menu" aria-labelledby="information">
-                    <li><Link className="dropdown-item" to="/AllActivity">กิจกรรมข่าว</Link></li>
-                    <li><Link className="dropdown-item" to="/DownloadDoc">เอกสารดาวน์โหลด</Link></li>
-                    <li><Link className="dropdown-item" to="/AllFAQ">คำถามที่พบบ่อย</Link></li>             
-                  </ul>
-                </li>
-
-                <li className="nav-item"><Link className="nav-link" to="/complaints">แจ้งเรื่องร้องเรียน</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/RelatedWebsite">เว็บไซต์ที่เกี่ยวข้อง</Link></li>
-              </ul>
+                        <li className="nav-item"><Link className="nav-link" to="/complaints">แจ้งเรื่องร้องเรียน</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/RelatedWebsite">เว็บไซต์ที่เกี่ยวข้อง</Link></li>
+                    </div>
+                  </div>
+                </nav>
             </div>
-          </div>
         </div>
-      </nav>
     </>
   );
 };
